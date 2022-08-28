22ba314bdb2f5358534b9cfbfcd492f9599dfe94
de-marauder <ezikegodson@gmail.com>
2022-08-28 08:13:48 +0100

    updates

--- assignment-1/map-component/src/components/cleanMapData.js
+++ assignment-1/map-component/src/components/cleanMapData.js
@@ -1,22 +1,31 @@
-import tweetData from '../assets/tweets data.json'
-import countryGeodata from '../assets/country_geodata/world_country_and_usa_states_latitude_and_longitude_values.json'
+// import tweetData from '../assets/tweets data.json'
+// import countryGeodata from '../assets/country_geodata/world_country_and_usa_states_latitude_and_longitude_values.json'
 
 // Get country name dataset
 export const getCountries = async () => {
     const proxyURL = 'https://still-lake-77123.herokuapp.com/'
     const targetURL = 'http://country.io/names.json'
-    const data = await (await fetch(proxyURL + targetURL)).json();
+    const data = await fetch(proxyURL + targetURL).then((data) => {
+        return data.json();
+    })
     return data;
 }
 // Get country code dataset
 export const getCountriesCodes = async () => {
     const proxyURL = 'https://still-lake-77123.herokuapp.com/'
     const targetURL = 'http://country.io/iso3.json'
-    const data = await (await fetch(proxyURL + targetURL)).json();
+    const data = await fetch(proxyURL + targetURL).then((data) => {
+        return data.json();
+    })
     return data;
 }
 
-export const cleanData = (countries, countriesCode) => {
+export const cleanData = async (countries, countriesCode) => {
+
+    let tweetData = await fetch('../assets/tweets data.json').then((data) => {return data.json()})
+    console.log(tweetData)
+    let countryGeodata = await fetch('../assets/country_geodata/world_country_and_usa_states_latitude_and_longitude_values.json').then((data) => {return data.json()})
+
     // Clean tweetData json (filter out junk countries and sentiments)
 
     const countryList = Object.keys(countries).map((el, id) => {
@@ -24,26 +33,26 @@ export const cleanData = (countries, countriesCode) => {
             code2: el,
             code3: Object.entries(countriesCode)[id][1],
             name: Object.entries(countries)[id][1],
-            long: countryGeodata.find((data)=> data["country_code"] === el)?.longitude,
-            lat: countryGeodata.find((data)=> data["country_code"] === el)?.latitude
+            long: countryGeodata.find((data) => data["country_code"] === el)?.longitude,
+            lat: countryGeodata.find((data) => data["country_code"] === el)?.latitude
         }
     })
     var filteredData = tweetData.data.twitter.data.filter((data) => {
-        
+
         return (
             data.location !== '' &&
             (// Check if any word in the location string is a valid country and introduced a new parameter 'locationCode' into tweets
-                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.code2 }).includes(el) }).map((bool, id) => { if(bool){data.locationCode = countryList.find((co)=> co.code2 === data.location.split(', ')[id]).code2}; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
-                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.code3 }).includes(el) }).map((bool, id) => { if(bool){data.locationCode = countryList.find((co)=> co.code3 === data.location.split(', ')[id]).code2}; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
-                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.name }).includes(el) }).map((bool, id) => { if(bool){data.locationCode = countryList.find((co)=> co.name === data.location.split(', ')[id]).code2}; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
+                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.code2 }).includes(el) }).map((bool, id) => { if (bool) { data.locationCode = countryList.find((co) => co.code2 === data.location.split(', ')[id]).code2 }; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
+                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.code3 }).includes(el) }).map((bool, id) => { if (bool) { data.locationCode = countryList.find((co) => co.code3 === data.location.split(', ')[id]).code2 }; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
+                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.name }).includes(el) }).map((bool, id) => { if (bool) { data.locationCode = countryList.find((co) => co.name === data.location.split(', ')[id]).code2 }; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
                 // Check for uppercase
-                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.code2 }).includes(el.toUpperCase()) }).map((bool, id) => { if(bool){data.locationCode = countryList.find((co)=> co.code2 === data.location.split(', ')[id]).code2}; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
-                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.code3 }).includes(el.toUpperCase()) }).map((bool, id) => { if(bool){data.locationCode = countryList.find((co)=> co.code3 === data.location.split(', ')[id]).code2}; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
-                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.name }).includes(el.toUpperCase()) }).map((bool, id) => { if(bool){data.locationCode = countryList.find((co)=> co.name === data.location.split(', ')[id]).code2}; return bool ? 1 : 0 }).reduce((a, b) => { return a + b })
+                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.code2 }).includes(el.toUpperCase()) }).map((bool, id) => { if (bool) { data.locationCode = countryList.find((co) => co.code2 === data.location.split(', ')[id]).code2 }; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
+                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.code3 }).includes(el.toUpperCase()) }).map((bool, id) => { if (bool) { data.locationCode = countryList.find((co) => co.code3 === data.location.split(', ')[id]).code2 }; return bool ? 1 : 0 }).reduce((a, b) => { return a + b }) ||
+                data.location.split(', ').map((el) => { return countryList.map((item) => { return item.name }).includes(el.toUpperCase()) }).map((bool, id) => { if (bool) { data.locationCode = countryList.find((co) => co.name === data.location.split(', ')[id]).code2 }; return bool ? 1 : 0 }).reduce((a, b) => { return a + b })
             )
         );
     })
-    
+
     const filteredDataLocations = filteredData.map((item) => {
         return item.locationCode
     })
@@ -54,31 +63,33 @@ export const cleanData = (countries, countriesCode) => {
 
     for (let [key, filteredDataLocation] of Object.entries(filteredDataLocations)) {
         if (locationCountList.length === 0) {
-            countryList.find((d)=> {
+            countryList.find((d) => {
                 return d === filteredDataLocation
             })
             locationCountList.push({
                 locationCode: filteredDataLocation,
-                location: countryList.find((d)=> d.code2 === filteredDataLocation)?.name,
-                latitude: countryList.find((d)=> d.code2 === filteredDataLocation)?.lat,
-                longitude: countryList.find((d)=> d.code2 === filteredDataLocation)?.long,
+                location: countryList.find((d) => d.code2 === filteredDataLocation)?.name,
+                latitude: countryList.find((d) => d.code2 === filteredDataLocation)?.lat,
+                longitude: countryList.find((d) => d.code2 === filteredDataLocation)?.long,
                 count: 1,
                 average: [filteredData[key].sentimentPolarity]
             })
             continue;
         }
 
-        let data = locationCountList[locationCountList.length - 1];
         const location = locationCountList.map((el) => el.locationCode)
         if (location.includes(filteredDataLocation)) {
+            const data = locationCountList.find((item)=>{
+                return item.locationCode === filteredDataLocation
+            });
             data.count += 1;
             data.average = [...data.average, filteredData[key].sentimentPolarity]
         } else {
             locationCountList.push({
                 locationCode: filteredDataLocation,
-                location: countryList.find((d)=> d.code2 === filteredDataLocation)?.name,
-                latitude: countryList.find((d)=> d.code2 === filteredDataLocation)?.lat,
-                longitude: countryList.find((d)=> d.code2 === filteredDataLocation)?.long,
+                location: countryList.find((d) => d.code2 === filteredDataLocation)?.name,
+                latitude: countryList.find((d) => d.code2 === filteredDataLocation)?.lat,
+                longitude: countryList.find((d) => d.code2 === filteredDataLocation)?.long,
                 count: 1,
                 average: [filteredData[key].sentimentPolarity]
             })
@@ -95,5 +106,5 @@ export const cleanData = (countries, countriesCode) => {
     })
     console.log(locationCountList)
 
-    return locationCountList
+    return [locationCountList, filteredData]
 }
---
+++ assignment-1/map-component/src/components/modal.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react'
+import Sentiment from './sentiment';
+
+export default function Modal(props) {
+
+    const [show, setshow] = useState(false);
+
+    // Select tweets from particular location
+    const validTweets = props.tweetData.filter((item) => {
+        return item.locationCode === props.tweetLocation
+    })
+
+    const selectLocationDetails = props.avTweetDetails.find((item) => item.locationCode === props.tweetLocation)
+
+    const showMore = () => {
+        setshow(!show)
+    }
+
+    const tweetShort = (item) => <>{item.tweet.split('').splice(0, 70).join('')} ... <span onClick={showMore}>read more</span></>
+    const tweetFull = (item) => <>{item.tweet} <span onClick={showMore}>show less</span></>
+
+    return (
+        <div className='modal-wrapper'>
+            <div onClick={() => { props.setModal(!props.modalActive) }} className='backdrop'></div>
+            <div className='modal'>
+                <div className='title'>
+                    <h2>Tweets from {props.tweetLocation}</h2>
+                    <h2 className='title-sentiment'>Sentiment <Sentiment classOptions={['big']} polarity={selectLocationDetails.average} /></h2>
+                </div>
+                <div className='tweets'>
+                    {validTweets.map((item, id) => {
+                        console.log(item)
+                        return (
+                            <div key={id} className="tweet">
+                                <div className='tweet-title'>
+                                    <div>
+                                        <div className='profile-image'>
+                                            <img src={item.profile_image_url} alt='profile-pic' />
+                                        </div>
+                                        <p><strong>{item.name}</strong> <span>@{item.handler_name}</span></p>
+                                    </div>
+                                    <div className='date'>
+                                        <p>{item.date.split('T').join(', ')}</p>
+                                        <Sentiment classOptions={[]} polarity={item.sentimentPolarity} />
+                                    </div>
+                                </div>
+                                <div>
+                                    {!show ? tweetShort(item) : tweetFull(item)}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
